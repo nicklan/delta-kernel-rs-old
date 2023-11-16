7895a9437b23c44001ce0c9fe2d1308ab14377b4
Nick Lanham <nick@afternight.org>
2023-11-16 14:24:14 -0800

    take 1 on ffi

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -56,6 +56,8 @@ pub mod schema;
 pub mod snapshot;
 pub mod table;
 
+pub mod ffi;
+
 pub use actions::{types::*, ActionType};
 pub use error::{DeltaResult, Error};
 pub use expressions::Expression;
